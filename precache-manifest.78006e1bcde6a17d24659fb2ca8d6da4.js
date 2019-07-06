42c2dda8ec590fe04d5ef8c0e401c825f0c0fc8c
Neil Bateman <neilbatman@gmail.com>
2019-07-05 17:20:16 -0700

    Updates

---
+++ precache-manifest.78006e1bcde6a17d24659fb2ca8d6da4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb2184c4768e587000ea12cd4ace695b",
+    "url": "/neilbateman/tap-room-react/index.html"
+  },
+  {
+    "revision": "95dbd546cfeb1d603e60",
+    "url": "/neilbateman/tap-room-react/static/css/main.bda77fa2.chunk.css"
+  },
+  {
+    "revision": "5ab91c0eb532dff2b5b6",
+    "url": "/neilbateman/tap-room-react/static/js/2.6ed32ea3.chunk.js"
+  },
+  {
+    "revision": "95dbd546cfeb1d603e60",
+    "url": "/neilbateman/tap-room-react/static/js/main.1dbc3c64.chunk.js"
+  },
+  {
+    "revision": "b8760558b7e46fd2d52e",
+    "url": "/neilbateman/tap-room-react/static/js/runtime~main.6ab245dd.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/neilbateman/tap-room-react/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "43e7628369b34b1a73f233e04a0dbc3c",
+    "url": "/neilbateman/tap-room-react/static/media/shape.43e76283.svg"
+  }
+]);
